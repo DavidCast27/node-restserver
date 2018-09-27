07b2058433ec5dbc9fc40b64b58f4d5a54019535
David Castrillon <david.cast27@gmail.com>
2018-09-26 21:14:56 -0500

    cambiar usuario

--- server/config/config.js
+++ server/config/config.js
@@ -15,7 +15,7 @@ let urlDB;
 if (process.env.NODE_ENV === 'dev') {
     urlDB = 'mongodb://localhost:27017/cafe';
 } else {
-    urlDB = 'mongodb://cafe-user:Apolo1618@ds051635.mlab.com:51635/cafe-node';
+    urlDB = 'mongodb://cafe-user:1234qwer@ds051635.mlab.com:51635/cafe-node';
 }
 // urlDB = 'mongodb://cafe-user:Apolo1618@ds051635.mlab.com:51635/cafe-node';
 process.env.URLDB = urlDB;
